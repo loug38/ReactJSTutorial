ef12311e5ccf9b534cba20543dc2569ca96f0ab2
Lou George <loug38@gmail.com>
2016-09-12 16:50:13 -0700

    Got mutliple classes printing, about to start passing data around

--- public/scripts/myexample.js
+++ public/scripts/myexample.js
@@ -3,7 +3,30 @@ var CommentBox = React.createClass({
 	render: function(){
 		return(
 			<div className="commentBox">
-				This is a comment
+				<h1> Comments </h1>
+				<CommentList />
+				<CommentForm />
+				This is in comment box
+			</div>
+		);
+	}
+});
+
+var CommentList = React.createClass({
+	render: function(){
+		return(
+			<div className="commentList">
+				This is in comment list 
+			</div>
+		);
+	}
+});
+
+var CommentForm = React.createClass({
+	render: function(){
+		return(
+			<div className="commentForm">
+				This is a comment form
 			</div>
 		);
 	}
