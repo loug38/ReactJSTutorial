601e449b04ef3c2eab9e49e50013d117154ed34d
Lou George <loug38@gmail.com>
2016-09-12 17:24:06 -0700

    Working from JSON now.

--- public/scripts/myexample.js
+++ public/scripts/myexample.js
@@ -1,12 +1,34 @@
 
 var CommentBox = React.createClass({
+	getInitialState: function() {
+		return { data: [] };
+	},
+
+	loadCommentsFromServer: function() {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+
+	componentDidMount: function() {
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+	},
+
 	render: function(){
 		return(
 			<div className="commentBox">
 				<h1> Comments </h1>
-				<CommentList />
+				<CommentList data={this.state.data} />
 				<CommentForm />
-				This is in comment box
 			</div>
 		);
 	}
@@ -14,9 +36,17 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
 	render: function(){
+		var commentNodes = this.props.data.map(function(comment) {
+			return(
+				<Comment author={comment.author} key={comment.id}>
+					{comment.text}
+				</Comment>
+			);
+		});
+
 		return(
 			<div className="commentList">
-				This is in comment list 
+				{commentNodes}
 			</div>
 		);
 	}
@@ -32,7 +62,21 @@ var CommentForm = React.createClass({
 	}
 });
 
+var Comment = React.createClass({
+	render: function(){
+		return(
+			<div className="comment">
+				<h2 className="commentAuthor">
+					{this.props.author}
+				</h2>
+				{this.props.children}
+			</div>
+		);
+	}
+});
+
+
 ReactDOM.render(
-	<CommentBox />,
+	<CommentBox url="api/comments" pollInterval={2000} />,
 	document.getElementById('content')
 );
