497acab4a5a0974d6b1d2c4d56f533778ab62eb1
Lou George <loug38@gmail.com>
2016-09-12 17:38:45 -0700

    Set up input boxes and states, so things should update now

--- public/scripts/myexample.js
+++ public/scripts/myexample.js
@@ -1,9 +1,11 @@
 
 var CommentBox = React.createClass({
+	// Initialize states
 	getInitialState: function() {
 		return { data: [] };
 	},
 
+	// Pulls the data from json file, and checks for errors
 	loadCommentsFromServer: function() {
 		$.ajax({
 			url: this.props.url,
@@ -18,11 +20,14 @@ var CommentBox = React.createClass({
 		});
 	},
 
+	// Calls for getting the data pulled in from json once the component
+	// is ready to mount.
 	componentDidMount: function() {
 		this.loadCommentsFromServer();
 		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
 	},
 
+	// Simply the interface
 	render: function(){
 		return(
 			<div className="commentBox">
@@ -34,6 +39,7 @@ var CommentBox = React.createClass({
 	}
 });
 
+// The actual "structure" of the comments. Name + Comment.
 var CommentList = React.createClass({
 	render: function(){
 		var commentNodes = this.props.data.map(function(comment) {
@@ -52,16 +58,47 @@ var CommentList = React.createClass({
 	}
 });
 
+// The box to fill in to post a comment.
 var CommentForm = React.createClass({
+	getInitialState: function() {
+		return {
+			author: '',
+			text: '',
+		}
+	},
+
+	// These are automatically called from render if there's a change
+	// in the input, and the state gets automatically updated! Neat!
+	handleAuthorChange: function(e) {
+		this.setState({author: e.target.value});
+	},
+
+	handleTextChnage: function(e) {
+		this.setState({text: e.target.value});
+	},
+
 	render: function(){
 		return(
-			<div className="commentForm">
-				This is a comment form
-			</div>
+			<form className="commentForm">
+				<input 	type="text" 
+								placeholder="Your name" 
+								value={this.state.author}
+								onChange={this.handleAuthorChange}
+				/>
+				<input 	type="text" 
+								placeholder="Your comment here..."
+								value={this.state.author}
+								onChange={this.handleTextChnage}
+				/>
+				<input type="submit" value="Post" />
+			</form>
 		);
 	}
 });
 
+// The body of the comment. Prints a Name in h2 and the comment passed
+// this.props.children is:
+// <Comment author='Fox Moulder', id='123456'> {THIS_IS_THE_CHILDREN} </Comment>
 var Comment = React.createClass({
 	render: function(){
 		return(
@@ -76,6 +113,7 @@ var Comment = React.createClass({
 });
 
 
+// Called on initial render. This is kinda like the constructor.
 ReactDOM.render(
 	<CommentBox url="api/comments" pollInterval={2000} />,
 	document.getElementById('content')
