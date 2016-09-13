cffc87a4616b77ab2a68eff727d5b8d41f1e16ef
loug38 <loug38@gmail.com>
2016-09-13 16:48:08 -0700

    Added another project

---
+++ ReactJSPlayground/public/scripts/myexample.js
@@ -0,0 +1,35 @@
+
+var FilterableProductTable = React.createClass({
+	render: function(){
+		return(
+			<div>
+				<box>
+					{this.props.products[0].category}
+				</box>
+			</div>
+		);
+	}
+});
+
+var SearchBar = React.createClass({
+	render: function(){
+		return(
+			
+		);
+	}
+})
+
+var PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
+
+ReactDOM.render(
+	<FilterableProductTable products={PRODUCTS} />,
+	document.getElementById('content')
+);
