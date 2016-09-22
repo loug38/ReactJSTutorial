1278870e211800a18b30da28d2cf3ea68d4b0ad5
Lou George <loug38@gmail.com>
2016-09-22 14:24:22 -0700

    Text messages interfaced

--- interfaceTemp/public/scripts/myexample.js
+++ interfaceTemp/public/scripts/myexample.js
@@ -11,10 +11,20 @@ class MessageCenter extends React.Component{
 						Contacts 
 					</contactsContainer>
 					<messagesContainer>
-						Messages 
+						<Messages />
 					</messagesContainer>
 				</messagingContainer>
 			</span>
+		);
+	}
+}
+
+class Messages extends React.Component{
+	render(){
+		return(
+			<message>
+				<text> this is a sample message. </text>
+			</message>
 		);
 	}
 }
